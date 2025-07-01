70e1c4dbd04d30b0c8d89ea5bad4a334d4140559
shahalimshompod <shahalimsompod@gmail.com>
2025-07-01 11:12:52 +0600

    added user email and password validation api

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const app = express();
 // configuring dotenv
 require("dotenv").config();
 
+// importing crypto for hashing password
+const crypto = require("crypto");
+
 // declaring port
 const port = process.env.PORT || 3000;
 
@@ -32,7 +35,238 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    console.log("ready");
+    // connecting the server with database collections
+    const events = client.db("Evenzaa").collection("all_events");
+    const user_credentials = client
+      .db("Evenzaa")
+      .collection("user_credentials");
+    const user_data = client.db("Evenzaa").collection("user_data");
+
+    // function for hashing the password
+    const hashPassword = (password) => {
+      return crypto.createHash("sha256").update(password).digest("hex");
+    };
+
+    // function for generating token
+    const generateToken = () => {
+      return crypto.randomBytes(64).toString("hex");
+    };
+
+    // middlewares here "verifyToken"
+    const verifyUserToken = async (req, res, next) => {
+      try {
+        const authHeader = req.headers.authorization;
+
+        // check if there is any token or not
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+          return res
+            .status(401)
+            .send({ message: "Unauthorized: No token provided" });
+        }
+
+        // get the token from bearer
+        const token = authHeader.split(" ")[1];
+
+        // match the token from database
+        const user = await user_credentials.findOne({ userToken: token });
+        const userData = await user_data.findOne({ email: user.email });
+
+        // if the token is not matched then return forbidden
+        if (!user) {
+          return res.status(403).send({ message: "Forbidden: Invalid token" });
+        }
+
+        // if the token is valid then allow access
+        req.user = {
+          id: userData._id,
+          email: userData.email,
+          name: userData.name,
+          image: userData.image,
+          createdAt: userData.createdAt,
+        };
+
+        // allow access to next middleware or route
+        next();
+      } catch (error) {
+        console.error("Token verification failed:", error);
+        return res.status(500).send({ message: "Internal Server Error" });
+      }
+    };
+
+    // operations
+    // post operations here
+
+    // post operation for storing user credentials like email, password
+    app.post("/user_credential", async (req, res) => {
+      try {
+        // getting desired data from client side
+        const data = req.body;
+        const email = data.email;
+        const password = data.password;
+
+        // generating hash password from the password
+        const hashedPassword = hashPassword(password);
+
+        // checking if data is loaded from the client side
+        if (!data || !email || !password || !hashedPassword) {
+          return res.send({
+            message: "Something Went Wrong",
+            insertedId: null,
+          });
+        }
+
+        // checking if the user was existed
+        const query = { email: email };
+        const existingUserInCredential = await user_credentials.findOne(query);
+        const existingUserInData = await user_data.findOne(query);
+
+        // if user is already existed in both collection then return
+        if (existingUserInCredential || existingUserInData) {
+          return res.send({
+            message: "User is already exist with this Email!",
+            insertedId: null,
+          });
+        }
+
+        // now if the user unique then store the user credential (email, password)
+        const userCredentials = {
+          email,
+          password: hashedPassword,
+          userToken: null,
+        };
+
+        // check userCredential and then store the data
+        if (!userCredentials) {
+          return res.send({
+            message: "Something Went Wrong",
+            insertedId: null,
+          });
+        } else {
+          const result = await user_credentials.insertOne(userCredentials);
+          res.send(result);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    });
+
+    // post operation for storing user data like name, email, image, createdAt
+    app.post("/user_data", async (req, res) => {
+      try {
+        // getting desired data from client side
+        const data = req.body;
+        const name = data.name;
+        const email = data.email;
+        const image = data.image;
+
+        // checking if the data is in the server
+        if (!data || !name || !email) {
+          return res.send({
+            message: "Something Went Wrong",
+            insertedId: null,
+          });
+        }
+
+        // final user data
+        const userData = {
+          name,
+          email,
+          image,
+          createdAt: new Date(),
+        };
+
+        // checking if userdata broken or something
+        if (!userData) {
+          return res.send({
+            message: "Something Went Wrong",
+            insertedId: null,
+          });
+        } else {
+          const result = await user_data.insertOne(userData);
+          res.send(result);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    });
+
+    // post operation for authenticate user
+    app.post("/user_authentication", async (req, res) => {
+      try {
+        // getting desired data from client
+        const data = req.body;
+        const email = data.email;
+        const password = data.password;
+
+        // making hash password of incoming password from client
+        const hashedPassword = hashPassword(password);
+
+        // checking if data email and password exist or not if exist then go ahead if not then return
+        if (!data || !email || !password) {
+          return res.send({ message: "Something went wrong!", token: null });
+        }
+
+        // validating database password with given password
+        const query = { email: email };
+        const user = await user_credentials.findOne(query);
+
+        // if user do not exist then return
+        if (!user) {
+          return res.send({
+            message: "Invalid Email or Password",
+            token: null,
+          });
+        }
+
+        // if password do not match then return
+        if (hashedPassword !== user.password) {
+          return res.send({
+            message: "Invalid Email or Password",
+            token: null,
+          });
+        } else {
+          // let's generate access token
+          const token = generateToken();
+
+          //   updating the state in database
+          const updateResult = await user_credentials.updateOne(
+            { email: email },
+            { $set: { userToken: token } }
+          );
+
+          if (!updateResult.modifiedCount > 0) {
+            return res.send({ message: "Something went wrong!", token: null });
+          } else {
+            res.send({ message: "User successfully logged in", token: token });
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    });
+
+    // post operation for logout
+    app.post("/logout", verifyUserToken, async (req, res) => {
+      try {
+        // getting the token
+        const token = req.headers.authorization.split(" ")[1];
+
+        //   update userToken to null
+        const result = await user_credentials.updateOne(
+          { userToken: token },
+          { $set: { userToken: null } }
+        );
+
+        // if the user token is not null then return
+        if (!result.modifiedCount > 0) {
+          return res.send({ message: "Something went wrong!", token: token });
+        } else {
+          res.send({ message: "User Logged out successfully", token: null });
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
